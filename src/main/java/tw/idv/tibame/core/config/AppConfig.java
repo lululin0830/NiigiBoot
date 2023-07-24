5302b8c55800d7e489df72f0e328060c2c35b3fe
Lulu <lululin0830@gmail.com>
2023-07-24 19:11:46 +0800

    <核心組態更新>新增Redis連線設定、託管RedisTemplate、Gson

---
+++ src/main/java/tw/idv/tibame/core/config/AppConfig.java
@@ -0,0 +1,41 @@
+package tw.idv.tibame.core.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.google.gson.Gson;
+
+@Configuration
+public class AppConfig {
+
+	// RedisTemplate 
+	@Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+    
+    // Gson
+    @Bean
+    public Gson gson() {
+        return new Gson();
+    }
+    
+}
