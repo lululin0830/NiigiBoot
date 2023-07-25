50722f4ee274b2d150a752bf10efec566ff22f2f
Lulu <lululin0830@gmail.com>
2023-07-25 23:06:08 +0800

    <購物車-方法更新> 1. DAO修改參數型別 2. DAO新增sort()方法 3. DAO實作完成 4. Service 新增方法宣告 (加入、移除)

--- src/main/java/tw/idv/tibame/shoppingcart/service/ShoppingCartService.java
+++ src/main/java/tw/idv/tibame/shoppingcart/service/ShoppingCartService.java
@@ -1,11 +1,25 @@
 package tw.idv.tibame.shoppingcart.service;
 
+import com.google.gson.JsonObject;
+
 public interface ShoppingCartService {
 
 	/**
-	 * 購物車頁面預設載入全部商品
+	 * 購物車頁面載入全部商品
 	 * @param memberId
 	 * @return
 	 */
 	String init(String memberId);
+	
+	/**
+	 * 加入購物車
+	 */
+	String addToCart(JsonObject data);
+	
+	/**
+	 * 從購物車移除商品
+	 */
+	boolean removeFromCart(JsonObject data);
+	
+	
 }
