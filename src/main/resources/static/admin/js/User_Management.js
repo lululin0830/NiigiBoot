ee43d5a2235a181414b28bd15ef556cb4e22ef8e
Jeffrey <jeffrey821537@gmail.com>
2023-07-29 23:57:45 +0800

    <前端的新增使用者功能>完成

--- src/main/resources/static/admin/js/User_Management.js
+++ src/main/resources/static/admin/js/User_Management.js
@@ -1,3 +1,4 @@
+// 查詢
 const search = function () {
     console.log("來了");
 
@@ -45,7 +46,57 @@ const search = function () {
 
 console.log("讀到了")
 document.getElementById("search").addEventListener("click", search);
+// 新增
+(() => {
+	const add = document.querySelector('#add');
+	const msg = document.querySelector('#msg');
+	const userName = document.querySelector('#userName');
+	const userAcct = document.querySelector('#userAcct');
+	const password = document.querySelector('#password');
+	const inputs = document.querySelectorAll('input');
+	add.addEventListener('click', () => {
+		// const accLength = userName.value.length;
+		// if (accLength < 5 || accLength > 50) {
+		// 	msg.textContent = '帳號長度須介於5~50字元';
+		// 	return;
+		// }
 
+		// const pwdLength = password.value.length;
+		// if (pwdLength < 6 || pwdLength > 12) {
+		// 	msg.textContent = '密碼長度須介於6~12字元';
+		// 	return;
+		// }
+		msg.textContent = '';
+		fetch('http://localhost:8080/Niigi/Register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				userName: userName.value,
+				password: password.value,
+				userAcct: userAcct.value,
+			}),
+		})
+			.then(resp => resp.json())
+			.then(body => {
+				const { successful } = body;
+				if (successful) {
+					for (let input of inputs) {
+						input.disabled = true;
+					}
+					add.disabled = true;
+					msg.className = 'info';
+					msg.textContent = '註冊成功';
+				} else {
+					msg.className = 'error';
+					msg.textContent = '註冊失敗';
+				}
+			});
+	});
+
+})();
+// 顯示畫面
 const init = function () {
     fetch('http://localhost:8080/Niigi/UserController', {
         method: 'GET',
