7ab78b5d77dcad55c51b58e839700dc5ad52f4f0
Wang-Ya-Ling <rrr150100@gmail.com>
2023-08-07 11:46:54 +0800

    <檔案更新> ProductSpecController.java檔 1.新增上下架按鈕用 2.新增分頁用8個 3.新增分頁搜尋用8個

--- src/main/java/tw/idv/tibame/products/controller/ProductSpecController.java
+++ src/main/java/tw/idv/tibame/products/controller/ProductSpecController.java
@@ -1,10 +1,14 @@
 package tw.idv.tibame.products.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import tw.idv.tibame.products.DTO.ProductSpecManageDTO;
 import tw.idv.tibame.products.service.ProductSpecService;
@@ -15,26 +19,148 @@ public class ProductSpecController {
 	@Autowired
 	private ProductSpecService productSpecService;
 	
-	@GetMapping("/AllProductSpecManage")
-	public List<ProductSpecManageDTO> getAllProductSpecManage() throws Exception {
-		return productSpecService.getAllProductManage("S000000001");
+	
+	/////////////////以下是分頁用
+	@PostMapping("/AllProductSpecManage")
+	public List<ProductSpecManageDTO> getAllProductSpecManage(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getAllProductManage(registerSupplier);
+	}
+	
+	@PostMapping("/AllProductSpecTotal")
+	public Integer getAllProductSpecTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getAllProductTotal(registerSupplier);
 	}
 	
 	
-	@GetMapping("/SoldProductSpecManage")
-	public List<ProductSpecManageDTO> getSoldProductSpecManage() throws Exception {
-		return productSpecService.getSoldProductManage("S000000001");
+	@PostMapping("/SoldProductSpecManage")
+	public List<ProductSpecManageDTO> getSoldProductSpecManage(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getSoldProductManage(registerSupplier);
 	}
 	
-	@GetMapping("/UpStatusProductSpecManage")
-	public List<ProductSpecManageDTO> getUpStatusProductSpecManage() throws Exception {
-		return productSpecService.getStatusProductManage("0","S000000001");
+	@PostMapping("/SoldProductSpecTotal")
+	public Integer getSoldProductSpecTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getSoldProductTotal(registerSupplier);
 	}
 	
-	@GetMapping("/DownStatusProductSpecManage")
-	public List<ProductSpecManageDTO> getDownStatusProductSpecManage() throws Exception {
-		return productSpecService.getStatusProductManage("1","S000000001");
+	
+	
+	@PostMapping("/UpStatusProductSpecManage")
+	public List<ProductSpecManageDTO> getUpStatusProductSpecManage(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductManage("0",registerSupplier);
+	}
+	
+	@PostMapping("/UpStatusProductSpecTotal")
+	public Integer getUpStatusProductSpecTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductTotal("0",registerSupplier);
+	}
+	
+	
+	@PostMapping("/DownStatusProductSpecManage")
+	public List<ProductSpecManageDTO> getDownStatusProductSpecManage(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductManage("1",registerSupplier);
+	}
+	
+	@PostMapping("/DownStatusProductSpecTotal")
+	public Integer getDownStatusProductSpecTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductTotal("1",registerSupplier);
 	}
 	
 
+	/////////////////以下是分頁搜尋用
+	@PostMapping("/AllSearch")
+	public List<ProductSpecManageDTO> getAllSearch(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getAllProductManage(optionName,searchText,registerSupplier);
+	}
+	
+	@PostMapping("/AllSearchTotal")
+	public Integer getAllSearchTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getAllProductTotal(optionName,searchText,registerSupplier);
+	}
+	
+	
+	@PostMapping("/SoldSearch")
+	public List<ProductSpecManageDTO> getSoldSearch(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getSoldProductManage(optionName,searchText,registerSupplier);
+	}
+	
+	@PostMapping("/SoldSearchTotal")
+	public Integer getSoldSearchTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getSoldProductTotal(optionName,searchText,registerSupplier);
+	}
+	
+	
+	@PostMapping("/UpStatusSearch")
+	public List<ProductSpecManageDTO> getUpStatusSearch(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductManage(optionName,searchText,"0",registerSupplier);
+	}
+	
+	@PostMapping("/UpStatusSearchTotal")
+	public Integer getUpStatusSearchTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductTotal(optionName,searchText,"0",registerSupplier);
+	}
+	
+	
+	@PostMapping("/DownStatusSearch")
+	public List<ProductSpecManageDTO> getDownStatusSearch(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductManage(optionName,searchText,"1",registerSupplier);
+	}
+	
+	@PostMapping("/DownStatusSearchTotal")
+	public Integer getDownStatusSearchTotal(@RequestBody Map<String, String> requestData) throws Exception {
+		String optionName = requestData.get("optionName");
+		String searchText = requestData.get("searchText");
+		String registerSupplier = requestData.get("registerSupplier");
+		return productSpecService.getStatusProductTotal(optionName,searchText,"1",registerSupplier);
+	}
+	
+
+	
+	///以下是上下架按鈕用
+	@PostMapping("/StatusButton")
+    public Boolean updateProductSpecStatus(@RequestBody Map<String, Object> requestData) throws Exception {
+		List<Long>  productSpecIdsList  =  (List<Long>) requestData.get("productSpecIds");
+		String[] productSpecIds = new String[productSpecIdsList.size()];
+	      // 將 ArrayList 中的元素逐個裝入 String[] 陣列中
+        for (int i = 0; i < productSpecIdsList.size(); i++) {
+        	productSpecIds[i] = productSpecIdsList.get(i).toString();
+        }
+        String shelvesMemberId = (String) requestData.get("shelvesMemberId");
+        String shelvesStatus = (String) requestData.get("shelvesStatus");
+        return productSpecService.updateStatusButton(productSpecIds, shelvesMemberId, shelvesStatus);
+    }
+	
+	
+	
+	
+	
+	
 }
